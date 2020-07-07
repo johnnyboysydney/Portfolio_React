bf6a2cce63953f88fa84fcccc515e3a240028910
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-07-07 18:03:35 +1000

    Adding home landling page

---
+++ portfolio_react/src/home/PortfolioLanding.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ScrollToTop from 'react-scroll-up';
+import PortfolioList from "../elements/portfolio/PortfolioList";
+
+
+const PortfolioLanding = () => {
+    let title = 'About Me'
+        description = 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered <a href="#">alteration</a> in some form, by injected humour, or randomised words which dont look even slightly believable. If you are going to use a passage of Lorem Ipsum,';
+    const PostList = BlogContent.slice();
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+
+export default PortfolioLanding;
