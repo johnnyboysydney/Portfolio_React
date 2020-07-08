003cbc5c76518875662536b8790a89ebddfcf7cd
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-07-08 17:06:42 +1000

    setting out the layout of the portfolio

---
+++ portfolio_react/src/dark/PortfolioLanding.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import ScrollToTop from 'react-scroll-up';
+import { FiChevronUp } from "react-icons/fi";
+import Helmet from "../component/common/Helmet";
+import TextLoop from "react-text-loop";
+import HeaderThree from "../component/header/HeaderThree";
+import FooterTwo from "../component/footer/FooterTwo";
+import TabTwo from "../elements/tab/TabTwo";
+import ContactThree from "../elements/contact/ContactThree";
+import PortfolioList from "../elements/portfolio/PortfolioList";
+import ServiceList from "../elements/service/ServiceList";
+import BlogContent from "../elements/blog/BlogContent";
+
+const SlideList = [
+    {
+        textPosition: 'text-left',
+        category: 'Welcome to my World',
+        description: '',
+        buttonText: '',
+        buttonLink: ''
+    }
+]
+const PortfolioLanding = () => {
+    let title = 'About Me',
+        description = 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered <a href="#">alteration</a> in some form, by injected humour, or randomised words which dont look even slightly believable. If you are going to use a passage of Lorem Ipsum,';
+    const PostList = BlogContent.slice(0 , 3);
+    return (
+
+    )
+}
+
+
+
+
+export default PortfolioLanding;
