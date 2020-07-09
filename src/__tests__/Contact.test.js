970becc0833523bfc25632d034680897910a636c
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-07-10 00:50:55 +1000

    fixing the test still broken

--- src/__tests__/Contact.test.js
+++ src/__tests__/Contact.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Contact from '..components/Contact';
+import Contact from '../Contact';
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
